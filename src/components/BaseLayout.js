566a19685c5cac3b38aa0ac596542242ccca8438
shubham <75199716+whodilawar@users.noreply.github.com>
2024-03-12 20:55:08 +0530

    first commit

---
+++ src/components/BaseLayout.js
@@ -0,0 +1,49 @@
+import React, {useEffect, useState} from 'react';
+import Style from './BaseLayout.module.scss'
+import Navbar from "./Navbar";
+import Home from "./home/Home";
+import About from "./about/About";
+import Portfolio from "./portfolio/Portfolio";
+import {Route, Routes} from "react-router-dom";
+import {Box, Grid} from "@mui/material";
+
+export default function BaseLayout() {
+   let [darkMode, setDarkMode] = useState(false);
+
+   function handleToggleDarkMode() {
+      let oppositeOfCurrentDarkMode = !darkMode
+      console.log(oppositeOfCurrentDarkMode)
+      localStorage.setItem('darkMode', `${oppositeOfCurrentDarkMode}`)
+      setDarkMode(oppositeOfCurrentDarkMode)
+   }
+
+   useEffect(() => {
+      let detectedDarkMode = eval(localStorage.getItem('darkMode'));
+
+      if (detectedDarkMode) {
+         setDarkMode(detectedDarkMode)
+      } else {
+         localStorage.setItem('darkMode', 'false')
+      }
+   }, [])
+
+   return (
+      <Box className={darkMode ? Style.dark : Style.light}>
+         <Grid container display={'flex'} flexDirection={'column'} minHeight={'100vh'}
+               justifyContent={'space-between'}>
+            <Grid item>
+               <Navbar darkMode={darkMode} handleClick={handleToggleDarkMode}/>
+            </Grid>
+            <Grid item flexGrow={1}>
+               <Routes>
+                  <Route exact path={'/portfolio'} element={<Home/>}/>
+                  <Route exact path={'/about'} element={<About/>}/>
+                  <Route exact path={'/project'} element={<Portfolio/>}/>
+               </Routes>
+            </Grid>
+            
+         </Grid>
+      </Box>
+   )
+}
+
---
+++ src/info/Info.js
@@ -0,0 +1,135 @@
+import self from "../img/self.png"
+import mock1 from "../img/mock1.png"
+import mock2 from "../img/mock2.png"
+import mock3 from "../img/mock3.png"
+import mock4 from "../img/mock4.png"
+import mock5 from "../img/mock5.png"
+
+/* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
+how to make this template work best for you! */
+
+export let colors = ["rgb(0,255,164)", "rgb(166,104,255)"];
+/*
+I highly recommend using a gradient generator like https://gradientgenerator.paytonpierce.dev/ to generate a pair of colors that you like.
+These colors will be used to style your name on the homepage, the background of your picture, and some other accents throughout
+the site.
+ */
+
+
+/*
+So let's get started! Some of the info below is pretty self-explanatory, like 'firstName' and 'bio'. I'll try to explain anything
+that might not be obvious right off the bat :) I recommend looking at the template example live using "npm start" to get an idea
+of what each of the values mean.
+ */
+
+export const info = {
+    firstName: "Shubham",
+    lastName: "Vishwakarma",
+    initials: "sv", // the example uses first and last, but feel free to use three or more if you like.
+    position: "a Mern Stack Developer",
+    selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
+    gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
+    baseColor: colors[0],
+    miniBio: [ // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
+        {
+            emoji: '☕',
+            text: 'fueled by coffee'
+        },
+        {
+            emoji: '🌎',
+            text: 'based in the India'
+        },
+        {
+            emoji: "💼",
+            text: "Software Engineer at HCL Tech"
+        },
+        {
+            emoji: "📧",
+            text: "vishwakarmashubham9111@gmail.com"
+        }
+    ],
+    socials: [
+        {
+            link: "https://facebook.com",
+            icon: 'fa fa-facebook',
+            label: 'facebook'
+        },
+        {
+            link: "https://github.com/whodilawar",
+            icon: "fa fa-github",
+            label: 'github'
+        },
+        {
+            link: "https://www.linkedin.com/in/shubham-vishwakarma-a1571151",
+            icon: "fa fa-linkedin",
+            label: 'linkedin'
+        },
+        {
+            link: "https://twitter.com",
+            icon: "fa fa-twitter",
+            label: 'twitter'
+        }
+// Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
+// Just change the links so that they lead to your social profiles.
+
+    ],
+    bio: "Hello! I'm shubham. I'm a software engineer for HCL Tech. I studied CompSci at sagar institute of research and technology excellence, I enjoy long walks on the beach, and I believe artificial intelligence will inevitably rule us all one day. You should hire me!",
+    skills:
+        {
+            proficientWith: ['javascript', 'react', 'git', 'github', 'bootstrap', 'html5', 'css3', 'figma','manual testing','api intregation'],
+            exposedTo: ['node.js',"express.js", 'mongodb','firebase']
+        }
+    ,
+    hobbies: [
+        {
+            label: 'reading',
+            emoji: '📖'
+        },
+        {
+            label: 'theater',
+            emoji: '🎭'
+        },
+        {
+            label: 'movies',
+            emoji: '🎥'
+        },
+        {
+            label: 'cooking',
+            emoji: '🌶'
+        }
+// Same as above, change the emojis to match / relate to your hobbies or interests.
+// You can also remove the emojis if you'd like, I just think they look cute :P
+    ],
+    portfolio: [ // This is where your portfolio projects will be detailed
+        {
+            title: "Project 1",
+            live: "https://whodilawar.github.io/portfolio", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
+            source: "https://github.com/whodilawar", // this should be a link to the **repository** of the project, where the code is hosted.
+            image: mock1
+        },
+        {
+            title: "Project 2",
+            live: "https://whodilawar.github.io/portfolio",
+            source: "https://github.com/whodilawar",
+            image: mock2
+        },
+        {
+            title: "Project 3",
+            live: "https://whodilawar.github.io/portfolio",
+            source: "https://github.com/whodilawar",
+            image: mock3
+        },
+        {
+            title: "Project 4",
+            live: "https://whodilawar.github.io/portfolio",
+            source: "https://github.com/whodilawar",
+            image: mock4
+        },
+        {
+            title: "Project 5",
+            live: "https://whodilawar.github.io/portfolio",
+            source: "https://github.com/whodilawar",
+            image: mock5
+        }
+    ]
+}
